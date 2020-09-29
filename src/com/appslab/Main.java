eb4dd4d377c11cedb8fce1a69d5fb182f7659572
martinspilak <martinecko24@gmail.com>
2020-09-29 09:36:32 +0200

    Multiple

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,14 +1,21 @@
 package com.appslab;
+import java.util.Arrays;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isTriangle(2, 3, 4));
-        System.out.println(isTriangle(3, 4, 5));
-        System.out.println(isTriangle(4, 3, 8));
+        System.out.println(Arrays.toString(multiplyByLength(new int[] {2, 3, 1, 0})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{4, 1, 1})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[] {1, 0, 3, 3, 7, 2, 1})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[] {0})));
     }
-    private static boolean isTriangle(int a,int b, int c) {
-        if (a + b > c && a + c > b && b + c > a) return true;
-        else return false;
-    }
+    private static int[] multiplyByLength(int[] array) {
+        int length = array.length;
+        for (var i = 0; i < length; i++)
+        {
+            array[i] *= length;
+        }
+        return array;
+   }
 }
