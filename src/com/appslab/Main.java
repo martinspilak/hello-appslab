f14dcca60a4d4da37dc6e3ce1c9bf1047c5f2354
martinspilak <martinecko24@gmail.com>
2020-09-29 08:21:00 +0200

    oprava

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,9 +3,12 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        double a = 0.2;
-        double b = 0.4;
-        double c = 100;
-        System.out.println ((int)(c / a * (a + b)));
+        System.out.println(totalDistance(0.2, 0.4, 100.0));
+        System.out.println(totalDistance(0.3, 0.2, 25.0));
+        System.out.println(totalDistance(0.1, 0.1, 6.0));
     }
+    private static double totalDistance(double stepHeight, double stepLength, double towerHeight) {
+        var numOfSteps = (towerHeight / stepHeight) * (stepHeight + stepLength);
+        return ((int)numOfSteps);
+}
 }
