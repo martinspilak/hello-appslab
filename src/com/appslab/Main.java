447ff69f916b159f190a750c49b9ae475bc6a0d0
martinspilak <martinecko24@gmail.com>
2020-09-29 09:10:00 +0200

    Equality

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,18 +3,12 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Equality(3, 4, 3));
-        System.out.println(Equality(1, 1, 1));
-        System.out.println(Equality(3, 4, 1));
+        System.out.println(isTriangle(2, 3, 4));
+        System.out.println(isTriangle(3, 4, 5));
+        System.out.println(isTriangle(4, 3, 8));
     }
-    private static double Equality(int a,int b, int c) {
-        if (a == b && b == c ) {
-            return (3);
-        }
-        else if(a == b || a == c || b == c){
-         return(2);}
-        else{
-        }
-        return (0);
-}
+    private static boolean isTriangle(int a,int b, int c) {
+        if (a + b > c && a + c > b && b + c > a) return true;
+        else return false;
+    }
 }
