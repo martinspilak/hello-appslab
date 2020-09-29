c5fdd8fbccf518ad9b83d94e1ee4a262043706c9
martinspilak <martinecko24@gmail.com>
2020-09-29 10:10:33 +0200

    Palindrome

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(equalSlices(11, 5, 2));
-        System.out.println(equalSlices(11, 5, 3));
-        System.out.println(equalSlices(8, 3, 2));
-        System.out.println(equalSlices(8, 3, 3));
-        System.out.println(equalSlices(24, 12, 2));
+        System.out.println(checkPalindrome("mom"));
+        System.out.println(checkPalindrome("scary"));
+        System.out.println(checkPalindrome("reviver"));
+        System.out.println(checkPalindrome("stressed"));
     }
-    private static boolean equalSlices(int totalslices, int people, int slicesforpople) {
-    if(people * slicesforpople <= totalslices) return true;
-    else return false;
+    private static boolean checkPalindrome(String str) {
+        StringBuilder sb=new StringBuilder(str);
+        if (sb.reverse().toString().equals(str)) return true;
+        else return false;
     }
 }
