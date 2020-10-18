d31a7696aea2b0c63c8699392a88457d5dd6c6da
martinspilak <martinecko24@gmail.com>
2020-10-18 17:05:18 +0200

    checkEnding

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -5,22 +5,15 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println((warOfNumbers(new int[]{2, 8, 7, 5})));
-        System.out.println((warOfNumbers(new int[]{12, 90, 75})));
-        System.out.println((warOfNumbers(new int[]{5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243})));
-        System.out.println((warOfNumbers(new int[]{7, 18})));
+        System.out.println(checkEnding("abc", "bc"));
+        System.out.println(checkEnding("abc", "d"));
+        System.out.println(checkEnding("samurai", "zi"));
+        System.out.println(checkEnding("feminine", "nine"));
+        System.out.println(checkEnding("convention", "tio"));
     }
 
-    private static int warOfNumbers(int[] array) {
-        int even = 0, odd = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0)
-                even += array[i];
-            else
-                odd += array[i];
-
-        }
-        if (even > odd) return even - odd;
-        else return odd - even;
+    private static Boolean checkEnding(String str1, String str2) {
+       if (str1.endsWith(str2)) return true;
+       else return false;
     }
 }
