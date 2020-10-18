1aad793da3b239be2f673bcac61bbd3aedf6e448
martinspilak <martinecko24@gmail.com>
2020-10-18 16:48:35 +0200

    WarOfNumbers oprava

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -8,18 +8,19 @@ public class Main {
         System.out.println((warOfNumbers(new int[]{2, 8, 7, 5})));
         System.out.println((warOfNumbers(new int[]{12, 90, 75})));
         System.out.println((warOfNumbers(new int[]{5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243})));
-        System.out.println((warOfNumbers(new int[]{7})));
+        System.out.println((warOfNumbers(new int[]{7, 18})));
     }
 
     private static int warOfNumbers(int[] array) {
         int even = 0, odd = 0;
         for (int i = 0; i < array.length; i++) {
-            if (i % 2 == 0)
+            if (array[i] % 2 == 0)
                 even += array[i];
             else
                 odd += array[i];
 
         }
-        return odd;
+        if (even > odd) return even - odd;
+        else return odd - even;
     }
 }
