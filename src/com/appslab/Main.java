e1ff66caced134512271926ab43bf3fa92fddcde
martinspilak <martinecko24@gmail.com>
2020-11-03 22:13:25 +0100

    Triangle oprava

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,11 +3,11 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        Person[] obj = new Person[]{
-                new Person("John", 21, 23000),
-                new Person("Steve", 32, 40000),
-                new Person("Martin", 16, 2700)
-        };
-        System.out.println(obj[0].getBudget() + obj[1].getBudget() + obj[2].getBudget());
+        Triangle triangle = new Triangle();
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        }
+
+
     }
-}
+
